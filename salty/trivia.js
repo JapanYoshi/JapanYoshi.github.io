2da8d8dd79d53fb0a9db30b84d81ee11e48660f1
JapanYoshi <japanyoshi777@gmail.com>
2019-11-30 18:53:32 +0900

    Update trivia.js

--- salty/trivia.js
+++ salty/trivia.js
@@ -567,8 +567,8 @@ function getNextPlayerName(index){
     snd.stopBgm(1500);
     console.log("game started");
   } else {
-    t.activate(i, 
-      params.presentList[i] < 0 ? 
+    t.activate(index,
+      params.presentList[index] < 0 ? 
         (params.isMobile ? 2 : 0)
       : 1, "Player " + (i+1) + ", enter your name"
     );
